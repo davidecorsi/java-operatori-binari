0ea3d5955f16cec73e4b07ed5b0607beba452b8a
Davide Corsi <davide.corsi@par-tec.it>
2021-09-30 15:21:21 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int i1 = 0, i2 = 0, i3 = 0, i4 = 0;
+		double d1 = 0, d2 = 0, d3 = 0, d4 = 0;
+		String s1 = "", s2 = "", s3 = "";
+		char c1 = ' ', c2 = ' ', c3 = ' ';
+		
+		// operatore di addizione
+		i1 = 5 + 3;
+		i2 =  6 +  i1;
+		i3 = i1 + i2;
+		
+		i1 = 3 + 6 + 8 + 10;
+		i2 = (12 + 6) * 2 * (i3 - (i4 / 5));
+		
+		i1 = 1000 * 1000 * 1000 * 1000;
+		System.out.println(i1);
+		
+		c1 = 'A';
+		c2 = ' ';
+		
+		c3 = 'A' + ' ';
+		System.out.println(c3);
+//		c3 = c1 + c2;
+		
+		// operatore di sottrazione
+//		i1 = - 1000 * 1000 * 1000 * 1000;
+//		System.out.println(i1);
+//		
+//		c3 = 'z' - 'A';
+//		System.out.println(c3);
+		
+		// operatore di moltiplicazione
+//		c1 = 3;
+//		c2 = ' ';
+//		c3 = (char) (c1 * c2);
+//		System.out.println(c3);
+		
+		// operatore di divisione
+//		i1 = 5 / 3;
+//		System.out.println(i1);
+		
+//		i1 = 40 / 15;
+//		System.out.println(i1);
+		
+//		double d5 = 40 / 15;
+//		System.out.println(d5);
+		
+//		c1 = 3;
+//		c2 = 'a';
+//		c3 = (char) (c2 / c1);
+//		System.out.println(c3);
+		
+		// somme di tipo diverso
+//		d1 = 100 + 34.81;
+//		i1 = 100 + 34.81;
+//		System.out.println(d1);
+//		System.out.println(i1);
+	}
+}
